2648630f5ebe6bef2332fd220df3ded70cdb58c9
Nikita Petko <petko@vmminfra.net>
2022-07-19 04:15:50 +0100

    Fix another issue

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("cxx20")
 
-includes("./src")
+includes("src")
 
 task("bundle")
     set_menu {
@@ -17,8 +17,8 @@ task("bundle")
 
 target("test-app")
     set_kind("binary")
-    add_headerfiles("**.h")
-    add_files("**.cc")
+    add_headerfiles("src/**.h")
+    add_files("src/**.cc")
     add_includedirs(".")
 	
 	if is_mode("debug") then
