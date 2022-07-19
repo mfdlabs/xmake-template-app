1de8b224d55a800fc6aed0f4b25fb9daaf300d6a
Nikita Petko <petko@vmminfra.net>
2022-07-19 08:37:32 +0100

    Revert XTA-2
    Have to do this because it need it???

--- xmake.lua
+++ xmake.lua
@@ -14,17 +14,3 @@ task("bundle")
         import("scripts.bundle")
         bundle()
     end)
-
-target("test-app")
-    set_kind("binary")
-    add_headerfiles("src/**.h")
-    add_files("src/**.cc")
-    add_includedirs(".")
-	
-	if is_mode("debug") then
-        set_symbols("debug")
-	end
-
-    if is_mode("release") then
-        set_optimize("fastest")
-    end
